6a62541fad8d768f424b9466a2843c6e0dee47d9
zaelani23 <mohamadzaelani09@gmail.com>
2021-06-01 00:40:43 +0700

    app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -21,3 +21,6 @@ app.register_blueprint(authentication, url_prefix="/api/auth")
 
 from Blueprints.blueprint_dashboard import dashboard
 app.register_blueprint(dashboard, url_prefix="/api/")
+
+if __name__ == "__main__":
+	app.run()
